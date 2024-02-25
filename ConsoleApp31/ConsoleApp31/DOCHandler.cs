224385f06fd34d243e6a7b8acc38e3191e099a47
Kaifarikzxc <147406196+Kaifarikzxc@users.noreply.github.com>
2024-02-25 22:23:50 +0300

    Add files via upload

---
+++ ConsoleApp31/ConsoleApp31/DOCHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class DOCHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Open DOC ");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine("Create DOC ");
+        }
+
+        public override void Change()
+        {
+            Console.WriteLine("Change DOC ");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("Save DOC ");
+        }
+    }
+}
---
+++ ConsoleApp31/ConsoleApp31/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+
+            AbstractHandler handler = new XMLHandler();
+            handler.Create();
+            handler.Open();
+            handler.Change();
+            handler.Save();
+            Console.WriteLine("\t");
+
+            AbstractHandler handler1 = new TXTHandler();
+            handler1.Create();
+            handler1.Open();
+            handler1.Change();
+            handler1.Save();
+            Console.WriteLine("\t");
+
+            AbstractHandler handler2 = new DOCHandler();
+            handler2.Create();
+            handler2.Open();
+            handler2.Change();
+            handler2.Save();
+            Console.WriteLine("\t \n");
+
+            Console.ReadKey();
+        }
+    }
+}
